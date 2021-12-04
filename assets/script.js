5f9d61043d231f6a71110e38a77cc1e66bae0abd
Sophie Doucet <asdoucet@berkeley.edu>
2021-12-03 17:50:19 -0800

    animation

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,25 @@
-function changepreviewart() {  
-    document.getElementById("art").classList.add('hidden');
+function changepreviewart() {
     document.getElementById("about").classList.add('hidden');
     document.getElementById("photo").classList.add('hidden');
     document.getElementById("design").classList.add('hidden');
-
     document.getElementById("art").classList.remove('hidden');
 }
 function changepreviewabout() {
     document.getElementById("art").classList.add('hidden');
-    document.getElementById("about").classList.add('hidden');
     document.getElementById("photo").classList.add('hidden');
     document.getElementById("design").classList.add('hidden');
-
     document.getElementById("about").classList.remove('hidden');
 }
 function changepreviewphoto() {
     document.getElementById("art").classList.add('hidden');
     document.getElementById("about").classList.add('hidden');
-    document.getElementById("photo").classList.add('hidden');
     document.getElementById("design").classList.add('hidden');
-
     document.getElementById("photo").classList.remove('hidden');
 }
 function changepreviewdesign() {
     document.getElementById("art").classList.add('hidden');
     document.getElementById("about").classList.add('hidden');
     document.getElementById("photo").classList.add('hidden');
-    document.getElementById("design").classList.add('hidden');
-
     document.getElementById("design").classList.remove('hidden');
 }
 
@@ -35,3 +27,9 @@ document.getElementById("nav-art").onmouseover= changepreviewart;
 document.getElementById("nav-about").onmouseover = changepreviewabout;
 document.getElementById("nav-photo").onmouseover = changepreviewphoto;
 document.getElementById("nav-design").onmouseover = changepreviewdesign;
+
+
+
+
+
+
